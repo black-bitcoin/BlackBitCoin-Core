b4c11f25439cc3404bacb9d8915a08c632fe202e
black-bitcoin <blackbitcoin@protonmail.com>
2019-10-07 16:32:05 +0530

    initial commit

---
+++ src/chain_conditional.h
@@ -0,0 +1,12 @@
+#ifndef CHAIN_CONDITIONAL_H
+#define CHAIN_CONDITIONAL_H
+
+extern bool fTestNet;
+
+static const int P1_End = 129600; // Total PoW Blocks
+static const int P1_End_TestNet = 30000;
+
+
+
+
+#endif // CHAIN_CONDITIONAL_H
---
+++ src/checkblocks.h
@@ -0,0 +1,16 @@
+#ifndef CHECKBLOCKS_H
+#define CHECKBLOCKS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+static const uint256 hashGenesisBlock("000008562b9a353e2256dacd85ddfe39b4e63e9299ba2852e014b249c7b9e09f");
+static const uint256 hashGenesisBlockTestNet("0000bfc0a4fd7d2215718380aae7e92f0821b0c4785a64039b173840ea1c36fb");
+
+// Chechpoints to ensure the correct chain usage during sync
+
+// Testnet Checkpoints
+//static const uint256 TestNetCheckBlock1 ("0000003b92febb5b04be82117fcbe7f1d422c305ddb4303e0d51ce32482d6304"); //Testnet Checkpoint at block 5000
+
+
+#endif // CHECKBLOCKS_H
